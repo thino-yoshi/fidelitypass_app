113e6e88b48f72312924c5cc897b419629b786a1
thino-yoshi <gobinthib99@gmail.com>
2026-09-12 18:58:59 +0200

    fix: add explicit R import in QartaWidgetProvider after package rename

--- android/app/src/main/kotlin/com/qarta/app/QartaWidgetProvider.kt
+++ android/app/src/main/kotlin/com/qarta/app/QartaWidgetProvider.kt
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import com.qarta.app.R
 
 class QartaWidgetProvider : AppWidgetProvider() {
 
